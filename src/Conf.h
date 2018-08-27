a8ac947b506a8f54c7349dc9421cd7e3819e1fdf
tucanae47 <cmsvalenzuela@gmail.com>
2018-08-27 16:11:01 -0500

    added barebone signing testing on esp32, although it seems signing is not working correctly yet

---
+++ src/Conf.h
@@ -0,0 +1,24 @@
+//
+// Created by Okada, Takahiro on 2018/02/03.
+//
+
+#ifndef ARDUINO_WEB3_CONSTANTS_H
+#define ARDUINO_WEB3_CONSTANTS_H
+
+#define ENV_SSID     ""
+#define ENV_WIFI_KEY ""
+
+const char* INFURA_HOST = "rinkeby.infura.io";
+const char* INFURA_PATH = "/v3/apitoken";
+
+const char * MY_ADDRESS = "0x185234bA42d395e1D7Fa04E273005d54c8a690C0";
+//const char* MY_ADDRESS = "0x185234ba42d395e1d7fa04e273005d54c8a690c0";
+//const char* CONTRACT_ADDRESS = "0xDC69E6deBd892f0d8f09aea4052430EaB57C18C7";
+const char* CONTRACT_ADDRESS = "0x03b42E164090cade396C57279eB5c52b5a99De30";
+
+
+
+const uint8_t PRIVATE_KEY[] = {
+  };
+
+#endif //ARDUINO_WEB3_CONSTANTS_H
