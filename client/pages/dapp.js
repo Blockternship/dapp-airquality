d5fd279d49d1de416fa75fc8be7a3870eeb70ca6
tucanae47 <cmsvalenzuela@gmail.com>
2018-09-06 21:32:58 -0500

    ui for doners, reporters and perks

--- client/pages/dapp.js
+++ client/pages/dapp.js
@@ -10,16 +10,24 @@ class Dapp extends React.Component {
   state = {
     balance: undefined,
     ethBalance: undefined,
-    eprice:0,
-    inputC:undefined
+    eprice:undefined,
+    inputC:undefined,
+    inputF:undefined,
+    inputfield:undefined
   };
-  inputChange = (e) => {
-	  console.log(e)
+  
+  async componentDidMount () {
+      this.getEthRates()
+      this.getValue()
+      this.getEthBalance()
   }
 
-  depositValue = async () => {
+
+  depositValue = async ( usd ) => {
     const { accounts, contract } = this.props
-    await contract.methods.depositEth().send( {value:"200000", from: accounts[0] })
+    const weiDeposit = (usd / this.state.eprice ) * 1e18
+    console.log(weiDeposit) 
+    await contract.methods.depositEth().send( {value:  weiDeposit , from: accounts[0] })
     alert('Stored 5 into account')
   }; 
   
@@ -31,7 +39,7 @@ class Dapp extends React.Component {
  
   addReporter = async () => {
     const { accounts, contract } = this.props
-    await contract.methods.addReporter("0x185234ba42d395e1d7fa04e273005d54c8a690c0").send({ from: accounts[0] })
+    await contract.methods.addReporter(this.state.inputfield).send({ from: accounts[0] })
     alert('Stored 5 into account')
   };
   
@@ -44,7 +52,7 @@ class Dapp extends React.Component {
   getEthRates = async () => {
     const eth = await fetch(`https://api.coinmarketcap.com/v1/ticker/ethereum/`).then( r => r.json()) 
     console.log(eth[0].price_usd)
-    this.setState({ eprice: parseInt(eth[0].price_usd) })
+    this.setState({ eprice: parseFloat(eth[0].price_usd) })
   };
 
   getValue = async () => {
@@ -59,39 +67,53 @@ class Dapp extends React.Component {
     this.setState({ ethBalance: balanceInWei / 1e18 })
   };
 
+  updateInputValue = async (evt) => {
+    //console.log("input field updated with "+evt.target.value);
+    this.setState({inputfield: evt.target.value})   
+  };
+
   render () {
     const { balance = 'N/A', ethBalance = 'N/A',eprice = 0, inputC ='N/A' } = this.state
     return (
  <Layout>
     <div style={{ marginTop: 100 }}>
       <Form layout='horizontal'>
-        <FormItem
-          label=''
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 8 }}
-        >
-		<Input onChange={this.inputChange} style={{ width: '50%' }} defaultValue="input content" />
-        </FormItem>
-	
-	<FormItem
-          label=''
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 8 }}
-        >
-		<Button onClick={this.depositValue} type="primary" size='large'>Primary</Button>
-		<Button onClick={this.getValue} type="primary" size='large'>Primary</Button>
-		<Button onClick={this.getEthRates} type="primary" size='large'>Dashed</Button>
-		<Button onClick={this.getReportBalance} type="primary" size='large'>Normal</Button>
-        </FormItem>
-
 	 <FormItem
-	  label=''
+	  label=' Your Doner Account'
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 8 }} 
 	 >
-		<Card title="Account" >Account Balance: {balance}</Card>
-		<Card title="Vault" >Ether Balance: {ethBalance}</Card>
-		<Card title="Eth usd" >Ether USD: {eprice}{inputC}</Card>
+		<Card title= {this.props.accounts} >Account Balance: { ethBalance} </Card>
+
+	</FormItem>
+
+        <FormItem
+          label='Input Address of reporter '
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 8 }}
+        >
+		<Input onChange={this.updateInputValue} style={{ width: '50%' }} defaultValue="input content" />
+
+		<Button onClick={this.addReporter} type="primary" size='large'>Add reporter</Button>
+        </FormItem>
+	
+	<FormItem
+          label=' Perks '
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 8 }}
+        >
+		<Button onClick={ ()=> this.depositValue(10)} type="primary" size='large'>Deposit 10</Button>
+		<Button onClick={()=> this.depositValue(20)} type="primary" size='large'>Deposit 20 </Button>
+		<Button onClick={()=> this.depositValue(50)} type="primary" size='large'>Deposit 40</Button>
+        </FormItem>
+
+	 <FormItem
+	  label='Air Quality Vault Balance'
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 8 }} 
+	 >
+		<Card title="Air Quality Vaul" >{balance}</Card>
+		<Card title="Eth usd" >Ether USD: {eprice}</Card>
 
 	</FormItem>
       </Form>
