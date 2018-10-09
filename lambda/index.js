4cbe148c4aa45c3667287d30014cf5b93da6ed6e
tucanae47 <cmsvalenzuela@gmail.com>
2018-10-08 21:50:25 -0500

    updated lambda and contract entrypoint

--- lambda/index.js
+++ lambda/index.js
@@ -4,15 +4,40 @@ const fs = require('fs');
 const path = require('path');
 const EthereumTx = require('ethereumjs-tx')
 const privateKey = Buffer.from(config.privateKey, 'hex')
-const address = '0x185234bA42d395e1D7Fa04E273005d54c8a690C0';
+//const address = '0x185234bA42d395e1D7Fa04E273005d54c8a690C0';
 const infura = config.infura;
 const web3 = new Web3(new Web3.providers.HttpProvider(infura));
 const ABI = JSON.parse(fs.readFileSync('./abi.json', 'utf8'));
-const rewards_contract = '0xbbdd97ba2c28807f0f6954897ce6971ce1ba0e2c';
+const rewards_contract = '0x98c05b41ede653adcb3959b01bfe7f6ccca8b65c';
 const contract = new web3.eth.Contract(ABI, rewards_contract);
-
+const yaml = require('js-yaml');
 
 exports.handler = function(event, context, callback) {
+
+
+    let body2dict = (body) => {
+        let dict = {};
+        while ((match = re.exec(body)) != null) {
+            dict[match[1]] = match[2];
+        }
+        return dict;
+
+    }
+    console.log(event);
+    let gh = event['body-json'];
+    let body = gh.issue.body;
+    let state = gh.issue.state;
+    let action = gh.action;
+    console.log(state,body,action);
+    let re = /\b(\S+?):(\S+?)\b/g;
+    var map = body2dict(body);
+    var address = map['address'];
+    console.log(address, map);
+
+    if (!state.includes('closed') || !action.includes('closed'))
+        return callback({}, {
+            message: 'invalid issue state'
+        });
 
     web3.eth.getTransactionCount(address).then((count) => {
 
@@ -25,13 +50,12 @@ exports.handler = function(event, context, callback) {
             value: '0x00',
             data: data,
             chainId: 4
-        }
+        };
         const tx = new EthereumTx(txParams)
         tx.sign(privateKey)
         const serializedTx = tx.serialize()
         web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
             .on('receipt', (receipt) => {
-                console.log(event.issue.state, event.issue.body);
                 callback(receipt, {
                     message: 'tx ok'
                 });
